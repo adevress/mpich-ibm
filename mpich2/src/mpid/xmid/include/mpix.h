d0e54c39e6a9df76a2144443a175239e9aa5368d
Joe Ratterman <jratt@us.ibm.com>
2010-01-20 16:20:28 -0600

    Do a better job of setting up paths and creating initial files

---
+++ mpich2/src/mpid/xmid/include/mpix.h
@@ -0,0 +1,22 @@
+/*  (C)Copyright IBM Corp.  2007, 2008  */
+/**
+ * \file include/mpix.h
+ * \brief Blue Gene extensions to the MPI Spec
+ *
+ * These functions generally use MPI functions and internal APIs to
+ * expose extra information relating to the specific system on which
+ * the job is running.  This may allow certain hardware specific
+ * optimizations to be made.
+ */
+
+#ifndef MPIX_H
+#define MPIX_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
