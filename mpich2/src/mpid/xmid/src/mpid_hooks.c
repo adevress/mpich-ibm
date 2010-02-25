c586e9e24e8f0fc849886200bce6c873aa73a5bb
Joe Ratterman <jratt@us.ibm.com>
2010-02-24 19:01:22 -0600

    Create dummy symbols for two RMA routines.
    Without these, almost none of BVT builts; with these, almost everything builds.

--- mpich2/src/mpid/xmid/src/mpid_hooks.c
+++ mpich2/src/mpid/xmid/src/mpid_hooks.c
@@ -14,3 +14,13 @@ void MPIDI_Comm_create(MPID_Comm *comm)
 void MPIDI_Comm_destroy(MPID_Comm *comm)
 {
 }
+
+
+
+int MPID_Dummy()
+{
+  assert(0);
+  return 1;
+}
+int MPID_Win_create()  __attribute__((alias("MPID_Dummy")));
+int MPID_Win_free()    __attribute__((alias("MPID_Dummy")));
