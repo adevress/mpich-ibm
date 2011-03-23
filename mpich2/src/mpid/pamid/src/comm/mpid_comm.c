1b5449b106344eaa6ad6aee6f52cff6f39a2d72c
Brian Smith <smithbr@us.ibm.com>
2011-03-23 15:19:25 -0500

    Need to revert this to check in some uncommited changes I had.
    Revert "fixing mpidcomm when there is no reduce for commsize=1"
    
    After further investigation the "real" fix changed slightly too.
    
    This reverts commit 182f7735d294efcf2b5812a13ca5f48865f76ddc.

--- mpich2/src/mpid/pamid/src/comm/mpid_comm.c
+++ mpich2/src/mpid/pamid/src/comm/mpid_comm.c
@@ -272,18 +272,11 @@ void MPIDI_Comm_coll_envvars(MPID_Comm *comm)
          continue;
 
       comm->mpid.user_selectedvar[i] = 0;
-      if(MPIDI_Process.verbose >= 1 && comm->rank == 0)
-        fprintf(stderr,"Setting up collective %d on comm %p\n", i, comm);
-      if(comm->mpid.coll_count[i][0] == 0 && comm->mpid.coll_count[i][1] == 0)
-      {
-        TRACE_ERR("There are no protocols of type %d. This *could* be a problem later.....\n", i);
-      }
-      else
-      {
-        comm->mpid.user_selected[i] = comm->mpid.coll_algorithm[i][0][0];
-        memcpy(&comm->mpid.user_metadata[i], &comm->mpid.coll_metadata[i][0][0],
-               sizeof(pami_metadata_t));
-      }
+         if(MPIDI_Process.verbose >= 1 && comm->rank == 0)
+            fprintf(stderr,"Setting up collective %d on comm %p\n", i, comm);
+      comm->mpid.user_selected[i] = comm->mpid.coll_algorithm[i][0][0];
+      memcpy(&comm->mpid.user_metadata[i], &comm->mpid.coll_metadata[i][0][0],
+            sizeof(pami_metadata_t));
    }
 
 
