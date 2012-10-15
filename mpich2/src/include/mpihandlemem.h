1cc6538f439308e2bd5ed9d6465cbbe7e6ad8c10
Bob Cernohous <bobc@us.ibm.com>
2012-10-14 19:36:47 -0500

    Issue 8178: Updated fix for ANL ticket 1645,reduces memory usage of that fix
    Decrease the new pool limit from
          - 8192K (1024 elements/block x 8192 blocks)
        to
          - 2048K (256 elements/block x 8192 blocks)
    
    Note before ticket 1645 (in V1R1M2) the pool limit was
          - 256K (256 elements/block x 1024 blocks)
    
    So this is an increase over V1R1M2 but a decrease from other (v1.5) MPICH
    platforms.
    
    I intentionally did not increase the elements/block at this time due to an
    earlier attempt's effect on gpaw -- an increment of 1024 new communicators
    in a pool was much too large.
    
    So although the total pool limit is increased (over V1R1M2) it will still only
    grow by 256 elements at a time (same as V1R1M2).
    
    While this limits the maximum pool size, we're unlikely on BGQ to support such
    large pools with our memory limitations.

--- mpich2/src/include/mpihandlemem.h
+++ mpich2/src/include/mpihandlemem.h
@@ -100,7 +100,7 @@ const char *MPIU_Handle_get_kind_str(int kind);
 #define HANDLE_NUM_BLOCKS 8192
 /* Number of objects in a block is bewtween 1 and 4096 (each obj has an index
  * within its block) */
-#define HANDLE_NUM_INDICES 1024
+#define HANDLE_NUM_INDICES 256
 
 /* For direct, the remainder of the handle is the index into a predefined 
    block */
