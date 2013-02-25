52ddfe18be5083d7c2799c7c3a7cc245d3b4354e
Michael Blocksome <blocksom@us.ibm.com>
2013-02-25 10:53:45 -0600

    Revert "Issue 7277: create mpif95 and mpif2003 compile scripts."
    This reverts commit fb37d2e434d9354210ada1162654de17e04b8cf7.
    
    I'm not sure this is fully baked yet. Also, its a very minor change so
    I'll pull it out until we determine it is needed.

--- mpich2/src/env/Makefile.mk
+++ mpich2/src/env/Makefile.mk
@@ -27,8 +27,8 @@ sysconf_DATA += src/env/mpif77.conf
 endif BUILD_F77_BINDING
 
 if BUILD_F90_LIB
-bin_SCRIPTS += src/env/mpif90 src/env/mpif95 src/env/mpif2003
-sysconf_DATA += src/env/mpif90.conf src/env/mpif95.conf src/env/mpif2003.conf
+bin_SCRIPTS += src/env/mpif90
+sysconf_DATA += src/env/mpif90.conf
 endif BUILD_F90_LIB
 
 if BUILD_CXX_LIB
@@ -46,10 +46,6 @@ src/env/mpif77: $(top_builddir)/src/env/mpif77.bash
 	cp -p $? $@
 src/env/mpif90: $(top_builddir)/src/env/mpif90.bash
 	cp -p $? $@
-src/env/mpif95: $(top_builddir)/src/env/mpif95.bash
-	cp -p $? $@
-src/env/mpif2003: $(top_builddir)/src/env/mpif2003.bash
-	cp -p $? $@
 else !BUILD_BASH_SCRIPTS
 src/env/mpicc: $(top_builddir)/src/env/mpicc.sh
 	cp -p $? $@
@@ -58,10 +54,6 @@ src/env/mpicxx: $(top_builddir)/src/env/mpicxx.sh
 src/env/mpif77: $(top_builddir)/src/env/mpif77.sh
 	cp -p $? $@
 src/env/mpif90: $(top_builddir)/src/env/mpif90.sh
-	cp -p $? $@
-src/env/mpif95: $(top_builddir)/src/env/mpif95.sh
-	cp -p $? $@
-src/env/mpif2003: $(top_builddir)/src/env/mpif2003.sh
 	cp -p $? $@
 endif !BUILD_BASH_SCRIPTS
 
@@ -72,16 +64,12 @@ DISTCLEANFILES += $(top_builddir)/src/env/cc_shlib.conf  \
                   $(top_builddir)/src/env/mpicc          \
                   $(top_builddir)/src/env/mpicxx         \
                   $(top_builddir)/src/env/mpif77         \
-                  $(top_builddir)/src/env/mpif90         \
-                  $(top_builddir)/src/env/mpif95         \
-                  $(top_builddir)/src/env/mpif2003
+                  $(top_builddir)/src/env/mpif90
 
 wrapper_doc_src = src/env/mpicc.txt \
                   src/env/mpif77.txt \
                   src/env/mpicxx.txt \
                   src/env/mpif90.txt \
-                  src/env/mpif95.txt \
-                  src/env/mpif2003.txt \
                   src/env/mpiexec.txt
 doc1_src_txt += $(wrapper_doc_src)
 EXTRA_DIST += $(wrapper_doc_src)
